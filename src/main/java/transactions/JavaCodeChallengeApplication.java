29c859cd669ee803d373b0ad704e0c30689a9b91
Martín Abramovich <abramovich.martin@gmail.com>
2026-04-16 23:28:34 -0300

    initial Spring Boot project structure

---
+++ src/main/java/transactions/JavaCodeChallengeApplication.java
@@ -0,0 +1,13 @@
+package transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCodeChallengeApplication.class, args);
+    }
+
+}
